effafd427f1acee8fb4951fd71738e14f4e64188
Franta1205 <hoza.fr@gmail.com>
2025-05-05 19:25:40 +0200

    adding project grid

--- app/javascript/vue/Components.ts
+++ app/javascript/vue/Components.ts
@@ -1,11 +1,13 @@
 import Greeting from './components/Greeting.vue'
 import TopBar from './components/navigation/TopBar.vue'
 import SideNav from './components/navigation/SideNav.vue'
+import ProjectGrid from './components/dashboard/ProjectGrid.vue'
 
 const components = {
   'Greeting': Greeting,
   "TopBar": TopBar,
-  "SideNav": SideNav, 
+  "SideNav": SideNav,
+  "ProjectGrid": ProjectGrid, 
 }
 
 export function getComponent(name) {
