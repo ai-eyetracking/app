bb0258c26443485c0311d2268e2db1d07b67defd
Franta1205 <hoza.fr@gmail.com>
2025-05-10 18:27:01 +0200

    add js from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "dash_board#index"
 
-  resources :projects, only: %i[new create]
+  resources :projects, only: %i[new create], export: true
 end
